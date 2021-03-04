67c1f381a70e319e411ab3599e69d239c527307c
Vomiann <karlyugin@gmail.com>
2021-03-04 21:01:19 +0200

    Added 10 variables

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTasksJunior
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte   maxLengthBoat;   // 1
+            sbyte  maxLevelDog;     // 2
+            short  playerDamage;    // 3
+            string namePlayer;      // 4
+            int    health;          // 5
+            uint   meatWeight;      // 6
+            long   playerDebt;      // 7
+            ulong  amountFirewood;  // 8
+            float  armorStrength;   // 9
+            double buildingCost;    // 10
+        }
+    }
+}
