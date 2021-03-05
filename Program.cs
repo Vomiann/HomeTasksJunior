d8f5e48c0190288adfa41c2d83f598cce0caad69
Vomiann <karlyugin@gmail.com>
2021-03-05 12:20:00 +0200

    update name of variable bool. remove comments

--- Program.cs
+++ Program.cs
@@ -10,17 +10,17 @@ namespace HomeTasksJunior
     {
         static void Main(string[] args)
         {
-            byte   maxLengthBoat;   // 1
-            sbyte  maxLevelDog;     // 2
-            short  playerDamage;    // 3
-            string namePlayer;      // 4
-            int    health;          // 5
-            uint   meatWeight;      // 6
-            long   playerDebt;      // 7
-            ulong  amountFirewood;  // 8
-            float  armorStrength;   // 9
-            double buildingCost;    // 10
-            bool   isDamagePlayer;  // 11
+            byte   maxLengthBoat;   
+            sbyte  maxLevelDog;     
+            short  playerDamage;    
+            string namePlayer;      
+            int    health;          
+            uint   meatWeight;      
+            long   playerDebt;      
+            ulong  amountFirewood;  
+            float  armorStrength;   
+            double buildingCost;    
+            bool   playerTookDamage;  
         }
     }
 }
