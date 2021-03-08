5be0e80d49deabd5a6463b2f9f15d0249990e2c3
Vomiann <karlyugin@gmail.com>
2021-03-08 21:25:19 +0200

    Added Lessons 3 (Images)

--- Lesson03_Images/Program.cs
+++ Lesson03_Images/Program.cs
@@ -10,14 +10,25 @@ namespace Lesson03_Images
     {
         static void Main(string[] args)
         {
+            int totalImages = 52;
+            int namberImagesRow = 3;
+            int pictureHeight = 4;
+            int pictureWidth = 4;
+            int specialScreenAreaHeight = 12;            
+            int specialScreenAreaWidth = 12;
+                     
+            Console.WriteLine($"Всего картинок:{totalImages}");
+            Console.WriteLine($"Высота картинки: {pictureHeight}, Ширина картинки:{pictureWidth}");
+            Console.WriteLine($"Специальная область экрана, где выводятся картинки имеет Высоту: {specialScreenAreaHeight} и Ширину: {specialScreenAreaWidth}");
 
-            //int screenHeight;
-            //int screenWidth;
-
-
+            int areaOneImage = pictureHeight * pictureWidth;
+            int areaOneRow = areaOneImage * namberImagesRow;
+            int imagesOutputArea = specialScreenAreaHeight * specialScreenAreaWidth;
             
+            Console.WriteLine($"Всего заполненных рядов картинок можно будет вывести: {imagesOutputArea / areaOneRow}");
+            Console.WriteLine($"Общее кол-во картинок за пределами области вывода:{ ((areaOneImage * totalImages) - imagesOutputArea) / areaOneImage }");
 
-
+            Console.ReadKey();
         }
     }
 }
