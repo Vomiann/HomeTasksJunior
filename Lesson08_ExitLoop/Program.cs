b90eea3406a5ce7334b696cd5e163e0c00178ac3
Vomiann <karlyugin@gmail.com>
2021-03-11 21:40:45 +0200

    Added Lesson08 ExitLoop

---
+++ Lesson08_ExitLoop/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson08_ExitLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string wordExit = "exit";
+            Console.Write(@"Введите ""ключевое"" слово для выхода из программы: ");
+            string userInput = Console.ReadLine();
+
+            while (true)
+            {
+                if (userInput == wordExit)
+                {
+                    Console.WriteLine(@"""Ключевое"" слово Принято! Вы успешно вышли из программы!");
+                    break;
+                }
+                Console.WriteLine(@"Не верно введено ""Ключевое"" слово! Вы не вышли из программы!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
