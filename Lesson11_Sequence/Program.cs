81f8f003e6c52c0fa873c746761e7cfcfa656f21
Vomiann <karlyugin@gmail.com>
2021-03-17 00:02:11 +0200

    Added Lesson11_Sequence

---
+++ Lesson11_Sequence/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson11_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberStep = 7;            
+            int totalNumber = 98;           
+
+            for (int i= 0; i <= totalNumber; i+= numberStep)
+            {
+                if (i != 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
