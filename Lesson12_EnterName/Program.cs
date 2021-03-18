2280f6fffb49ed132d76b07d3332ab49de21a4d2
Vomiann <karlyugin@gmail.com>
2021-03-18 02:11:30 +0200

    Added Lesson12 Enter Name

--- Lesson12_EnterName/Program.cs
+++ Lesson12_EnterName/Program.cs
@@ -10,22 +10,27 @@ namespace Lesson12_EnterName
     {
         static void Main(string[] args)
         {
+            int countSymbols = 2;
 
-            Console.WriteLine("Введите имя: ");
+            Console.Write("Введите имя: ");
             string inputName = Console.ReadLine();
 
-            Console.WriteLine("Введите символ: ");
+            Console.Write("Введите символ: ");
             string inputSymbol = Console.ReadLine();
 
             int lengthName = inputName.Length;
 
-            for (int i = 0; i < 3; i++)
+            for (int j = 0; j < lengthName + countSymbols; j++)
             {
-
-                Console.WriteLine($"");
+                Console.Write($"{inputSymbol}");
             }
+            Console.WriteLine();
+            Console.WriteLine($"{inputSymbol}"+ inputName + $"{inputSymbol}");
 
-
+            for (int j = 0; j < lengthName + countSymbols; j++)
+            {
+                Console.Write($"{inputSymbol}");
+            }
 
             Console.ReadKey();
         }
