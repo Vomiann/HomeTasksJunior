d81736b683053d7e65af37529d88df77341de9d3
Vomiann <karlyugin@gmail.com>
2021-03-21 17:25:10 +0200

    Added Lesson13 Password

---
+++ Lesson13_Password/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson13_Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countAttempts = 3;
+            string password = "password";
+            string secretMessage = "SecretMessage";
+            string inputUser;
+
+            while (countAttempts != 0)
+            {
+                Console.Write("Введите пароль: ");
+                inputUser = Console.ReadLine();
+
+                if (inputUser == password)
+                {
+                    Console.WriteLine($"Пароль принят! Секретное сообщение: {secretMessage}");                   
+                }
+                else
+                {
+                    Console.WriteLine($"Неверный пароль!");
+                    countAttempts--;
+                }
+            }
+            Console.WriteLine("Программа завершена! У вас закончились попытки");
+
+            Console.ReadKey();
+        }
+    }
+}
