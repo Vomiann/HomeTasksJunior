11857a9e9123b4880099a360aefc049a0a9e178d
Vomiann <karlyugin@gmail.com>
2021-03-24 01:15:58 +0200

    Added result Lesson15

--- Lesson15_CorrectStrings/Program.cs
+++ Lesson15_CorrectStrings/Program.cs
@@ -10,8 +10,33 @@ namespace Lesson15_CorrectStrings
     {
         static void Main(string[] args)
         {
-            int[,] ttt = new int[0,0];
+            int[,] array = {{1,2,3},{4,5,6},{7,8,9}};
+            int sumResult = 0;
+            int numberRow = 1;
+            int numberColumn = 0;
+            int multiplicationResult = 1; 
 
+            Console.WriteLine("Исходная матрица:");
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (i == numberRow)
+                    {
+                        sumResult += array[i, j];
+                    }
+                    if (j == numberColumn)
+                    {
+                        multiplicationResult *= array[i, j];
+                    }
+                    Console.Write(array[i,j] +" ");
+                }                
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"Сумма второй строки: {sumResult}");
+            Console.WriteLine($"Произведение первого столбца: {multiplicationResult}");
 
             Console.ReadKey();
         }
