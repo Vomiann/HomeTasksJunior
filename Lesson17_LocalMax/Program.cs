da4e2f052dc1c0e7cf4708651e231d24693cdc7b
Vomiann <karlyugin@gmail.com>
2021-03-26 01:36:13 +0200

    Added logic Lesson17 locMax

--- Lesson17_LocalMax/Program.cs
+++ Lesson17_LocalMax/Program.cs
@@ -10,8 +10,30 @@ namespace Lesson17_LocalMax
     {
         static void Main(string[] args)
         {
+            int[] array = new int[30];            
+            Random random = new Random();
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1,31);
+            }            
 
-
+            Console.WriteLine("Локальные максимумы: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i == 0 && array[i] > array[i+1])
+                {
+                    Console.Write(array[i] +" ");
+                }
+                else if (i != 0 && i!= array.Length - 1 &&  array[i] > array[i+1] && array[i] > array[i-1])
+                {
+                    Console.Write(array[i] + " ");
+                }
+                else if ( i == array.Length - 1 && array[array.Length - 1] > array[array.Length - 2])
+                {
+                    Console.Write(array[i] + " ");
+                }
+            }
 
             Console.ReadKey();
         }
