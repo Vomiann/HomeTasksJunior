54d152454d4436b135f1955ff2755822d4a0b3c4
Vomiann <karlyugin@gmail.com>
2021-03-30 00:29:26 +0300

    fix logic in Lesson 17

--- Lesson17_LocalMax/Program.cs
+++ Lesson17_LocalMax/Program.cs
@@ -12,27 +12,29 @@ namespace Lesson17_LocalMax
         {
             int[] array = new int[30];            
             Random random = new Random();
-            
+
             for (int i = 0; i < array.Length; i++)
             {
-                array[i] = random.Next(1,31);
-            }            
+                array[i] = random.Next(1, 31);
+            }
 
             Console.WriteLine("Локальные максимумы: ");
-            for (int i = 0; i < array.Length; i++)
+            if (array[0] > array[1])
             {
-                if (i == 0 && array[i] > array[i+1])
+                Console.Write(" "+array[0]);
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {                
+                if (i != 0 && i!= array.Length - 1 &&  array[i] > array[i+1] && array[i] > array[i-1])
                 {
-                    Console.Write(array[i] +" ");
-                }
-                else if (i != 0 && i!= array.Length - 1 &&  array[i] > array[i+1] && array[i] > array[i-1])
-                {
-                    Console.Write(array[i] + " ");
-                }
-                else if ( i == array.Length - 1 && array[array.Length - 1] > array[array.Length - 2])
-                {
-                    Console.Write(array[i] + " ");
-                }
+                    Console.Write(" " + array[i]);
+                }                
+            }
+
+            if (array[array.Length - 1] > array[array.Length - 2])
+            {
+                Console.Write(" " + array[array.Length - 1]);
             }
 
             Console.ReadKey();
