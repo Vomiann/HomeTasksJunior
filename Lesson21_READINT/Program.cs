57bd0ed85a852dd7c8e1c556894c2b29100ebc07
Vomiann <karlyugin@gmail.com>
2021-07-05 23:35:07 +0300

    Lesson21 readint final

---
+++ Lesson21_READINT/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lesson21_READINT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = ConvertToNumber();
+            Console.WriteLine($"Число: {result}");
+
+            Console.ReadKey();
+        }
+
+        static int ConvertToNumber()
+        {
+            int number = 0;
+            bool isNumber = false;
+
+            while (!isNumber)
+            {
+                Console.Write("Введите число: ");
+                string inputUser = Console.ReadLine();
+                if (int.TryParse(inputUser, out number))
+                {
+                    isNumber = true;
+                }
+            }
+
+            return number;
+        }
+    }
+}
