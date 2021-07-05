23b07887131625f61303bf3e06004aaa5872cdab
Vomiann <karlyugin@gmail.com>
2021-07-06 01:07:48 +0300

    Added Lesson22_BraveNewWorld final

---
+++ Lesson22_BraveNewWorld/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+
+namespace Lesson22_BraveNewWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            bool isPlaying = true;
+            int playerX, playerY;
+            int playerDX = 0, playerDY = 0;
+
+            char[,] map = ReadMap("map1", out playerX, out playerY);
+            DrawMap(map);
+
+            while (isPlaying)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+
+                    ChangeDirection(key, ref playerDX, ref playerDY);
+                }
+
+                if (map[playerX + playerDX, playerY + playerDY] != '#')
+                {
+                    Move(ref playerX, ref playerY, playerDX, playerDY);
+                }
+
+                System.Threading.Thread.Sleep(200);
+            }
+        }
+
+
+        static void ChangeDirection(ConsoleKeyInfo key, ref int DX, ref int DY)
+        {
+            switch (key.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    DX = -1; DY = 0;
+                    break;
+                case ConsoleKey.DownArrow:
+                    DX = 1; DY = 0;
+                    break;
+                case ConsoleKey.LeftArrow:
+                    DX = 0; DY = -1;
+                    break;
+                case ConsoleKey.RightArrow:
+                    DX = 0; DY = 1;
+                    break;
+            }
+        }
+
+        static void Move(ref int X, ref int Y, int DX, int DY)
+        {
+            Console.SetCursorPosition(Y, X);
+            Console.Write(" ");
+            X += DX;
+            Y += DY;
+
+            Console.SetCursorPosition(Y, X);
+            Console.Write('@');
+        }
+
+        static void DrawMap(char[,] map)
+        {
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    Console.Write(map[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static char[,] ReadMap(string mapName, out int playerX, out int playerY)
+        {
+            playerX = 0;
+            playerY = 0;
+
+            string[] newFile = File.ReadAllLines($"Maps/{mapName}.txt");
+            char[,] map = new char[newFile.Length, newFile[0].Length];
+
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    map[i, j] = newFile[i][j];
+
+                    if (map[i, j] == '@')
+                    {
+                        playerX = i;
+                        playerY = j;
+                    }
+                }
+            }
+            return map;
+        }
+    }
+}
