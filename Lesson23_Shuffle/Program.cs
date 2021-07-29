9ad557d1a14689024906540d3fc3d75c94dda69c
Vomiann <karlyugin@gmail.com>
2021-07-30 02:12:53 +0300

    Updated Lesson23 Shuffle

--- Lesson23_Shuffle/Program.cs
+++ Lesson23_Shuffle/Program.cs
@@ -8,75 +8,45 @@ namespace Lesson23_Shuffle
         {
             int[] array = new int[] { 1, 2, 3, 4, 5 };
 
-            for (int i = 0; i < array.Length; i++)
-            {
-                Console.Write(array[i]);
-            }
+            ShowArray(array);
 
             Console.WriteLine();
 
-            Shuffle(ref array);
+            int[] mixedArray = Shuffle(array);
 
-            for (int i = 0; i < array.Length; i++)
-            {
-                Console.Write(array[i]);
-            }
+            ShowArray(mixedArray);
 
             Console.ReadKey();
         }
 
 
-        static void Shuffle(ref int [] array)
-        {            
-            Random random = new Random();
-            int[] tempArray = new int[array.Length];
-            int[] tempIndexArray = new int[array.Length];
-            int rndIndex;
-
-            for (int i = 0; i < tempIndexArray.Length; i++)
-            {
-                rndIndex = random.Next(0, array.Length);
-
-                if (CheckIndexInArray(tempIndexArray, rndIndex) == false)
-                {
-                    tempIndexArray[i] = rndIndex;
-                }
-                else
-                {
-                    if (tempIndexArray[i] == 0)
-                    {
-                        for (int j = 0; j < array.Length; j++)
-                        {
-                            if (tempIndexArray[i] != array[j] && CheckIndexInArray(tempIndexArray, j) == false)
-                            {
-                                tempIndexArray[i] = j;                                
-                            }                            
-                        }
-                    }
-
-                }
-            }
-                      
-            for (int i = 0; i < tempArray.Length; i++)
-            {
-                tempArray[i] = array[tempIndexArray[i]];
-            }
-
-            array = tempArray;
-        }
-
-        static bool CheckIndexInArray(int[] array, int index)
+        static void ShowArray(int[] array)
         {
-            bool isIndex = false;
-
             for (int i = 0; i < array.Length; i++)
             {
-                if (array[i] == index)
+                Console.Write(array[i]);
+            }
+        }
+
+        static int[] Shuffle(int[] array)
+        {
+            Random random = new Random();
+            int randomIndex;                        
+            int tempValue;
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                randomIndex = random.Next(0, array.Length);
+
+                if (array[i] != array[randomIndex])
                 {
-                    isIndex = true;
+                    tempValue = array[randomIndex];
+                    array[randomIndex] = array[i];
+                    array[i] = tempValue;
                 }
             }
-            return isIndex;
+
+            return array;
         }
     }
 }
