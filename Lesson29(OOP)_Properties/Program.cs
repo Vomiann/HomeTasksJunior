5133b35f02280ba760b3f8162026d8eb8ad4926f
Vomiann <karlyugin@gmail.com>
2021-08-03 22:31:28 +0300

    Remove folder Lesson29 in root

---
+++ Lesson29(OOP)_Properties/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson29_OOP__Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            Draw draw = new Draw();
+            draw.DrawPlayer(player.X, player.Y);
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Player
+    {
+        public Player()
+        {
+            X = 10;
+            Y = 5;
+        }
+
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+    }
+
+    public class Draw
+    {        
+        public void DrawPlayer(int x, int y)
+        {
+            Console.SetCursorPosition(x,y);
+            Console.WriteLine("Player");
+        }
+    }
+}
